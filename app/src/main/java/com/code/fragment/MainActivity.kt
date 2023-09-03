69854a6988fee9233a836b5ff93468f76e58c9fd
lalubramadhan <lalubagoes2@gmail.com>
2023-09-03 23:51:19 +0700

    first commit

---
+++ app/src/main/java/com/code/fragment/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.code.fragment
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val fragmentManager = supportFragmentManager
+        val homeFragment = HomeFragment()
+        val fragment = fragmentManager.findFragmentByTag(HomeFragment::class.java.simpleName)
+
+        if (fragment !is HomeFragment){
+            Log.d("MyFragment", "Fragment name: " + HomeFragment::class.java.simpleName)
+            fragmentManager
+                .beginTransaction()
+                .add(R.id.frame_container, homeFragment, HomeFragment::class.java.simpleName)
+                .commit()
+        }
+    }
+}
